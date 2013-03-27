ecfa600743fdba31c31f5257c0b2172e5ee2c92e
Ivan Tyagov <ivan@nexedi.com>
2013-03-27 16:56:29 +0200

    Decrease wait.

--- product/Zelenium/selenium/core/scripts/selenium-testrunner.js
+++ product/Zelenium/selenium/core/scripts/selenium-testrunner.js
@@ -367,7 +367,7 @@ objectExtend(HtmlTestRunnerControlPanel.prototype, {
     },
 
     reset: function() {
-        this.runInterval = 2000; //this.speedController.value;
+        this.runInterval = 1000; //this.speedController.value;
         this._switchContinueButtonToPause();
     },
 
