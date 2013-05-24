f6e5a8431cf1a302303cbc096ed6a6cb26d91f50
Benjamin Blanc <benjamin.blanc@tiolive.com>
2013-05-24 09:41:10 +0000

    Change extends

--- erp5/util/testnode/testnode.py
+++ erp5/util/testnode/testnode.py
@@ -24,6 +24,9 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
+
+#Bonjour
+
 from datetime import datetime,timedelta
 import os
 import subprocess
